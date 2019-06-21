8e3b82311b692ea58280bcfd7610802b93de2154
Radharay <radharayg@gmail.com>
2019-06-21 16:10:20 -0300

    Semana 04 - Potência; Exemplo com while

--- Python/week04/While/pontecia.py
+++ Python/week04/While/pontecia.py
@@ -1,7 +1,7 @@
 #Repetição:
 
 #sem while
-
+'''
 print(2 ** 0)
 print(2 ** 1)
 print(2 ** 2)
@@ -16,4 +16,24 @@ print(2 ** 10)
 print(2 ** 11)
 print(2 ** 12)
 print(2 ** 13)
-    
+
+'''    
+
+'''
+while condição verdadeira:
+      condição1
+      condiçao2
+condiação 03 (se for falsa)
+
+Como reescrever de forma mais inteligente: Indentificar a parte constante e a parte variável.
+Constante: print(2 ** 
+Variável: o valor, por exemplo: i = 0
+'''
+
+# imprimir as 10 primeiras potências:
+
+i = 0
+
+while i <= 10:
+    print(2 ** i)
+    i = i + 1 # variando, incrementar
