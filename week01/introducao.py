433e67c799eade60f0b8a10d09c9375a51020888
Radharay <radharayg@gmail.com>
2019-06-21 14:09:35 -0300

    Semana 01 - Introdução

---
+++ week01/introducao.py
@@ -0,0 +1,68 @@
+#introdução 
+
+1 + 1
+#2
+8 * 3
+#24
+2 + 5 * 2
+#12
+(2 + 5) * 2
+#14
+1 - 4
+#-3
+(3 - (4 * 20 + ( 3 - (3 * 7))))
+#-59
+2 ** 3
+#8
+10 ** 6
+#1000000
+
+#comporações
+2 < 20
+#True
+10 > 5
+#True
+10 > 100
+#False
+5 <= 5
+#True
+
+9 ** 2 == 80 + 1
+#true
+
+
+20 != 30
+#True
+20 ** 2 != 400
+#false
+
+ 
+#variáveis
+
+x = 10
+print(x)
+#10
+
+y = 1 + 10
+print(y)
+#11
+x = y
+print (x)
+# 11
+
+soma = x + y
+print(soma)
+#22
+
+frase = "A massa ainda comerá do fino biscoito que fabrico"
+print(frase)
+
+peso = 78
+altura = 183
+print(peso)
+print(altura)
+
+a = 10
+b = 20
+soma = a + b
+print("A soma dos números é", soma)
